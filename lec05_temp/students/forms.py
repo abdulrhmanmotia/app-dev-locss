775efd136fda91837fa2117558261b1eff2617c9
abdua <bdalrhmansnan1@example.com>
2025-08-22 18:17:42 +0300

    حذف مجلد lec05_temp نهائيًا

--- lec05_temp/students/forms.py
+++
@@ -1,7 +0,0 @@
-# students/forms.py
-from django import forms
-
-class ThankYouForm(forms.Form):
-    student_name = forms.CharField(label='اسم الطالب', max_length=100)
-    email = forms.EmailField(label='البريد الإلكتروني')
-    message = forms.CharField(label='الرسالة', widget=forms.Textarea)
